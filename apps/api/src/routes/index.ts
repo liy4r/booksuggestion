91295b7142fc953230c3e5b2e9c221dac0979b51
liy4r <b.dulguun0114@gmail.com>
2026-04-06 20:12:07 +0800

    chore: removed needless import at routes

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { router as bookRouter } from './books';
 import { router as userRouter } from './user';
-import { AuthMiddleware } from '../middleware';
 
 export const router = Router();
 
